dc746494353bc76fa2a6ceaf0e53d4a6d0ac76ca
Jake <jake83@gmail.com>
2016-04-20 20:10:52 -0700

    build db method skeleton

--- server/db.js
+++ server/db.js
@@ -1,1 +1,34 @@
-var db = require('sqlite3');
+var sqlite3 = require('sqlite3').verbose();
+var path = require('path');
+
+var db = new sqlite3.Database(path.join(__dirname, '../db/jobascript.sqlite3'), function(err) {
+  if(err) {
+    console.error('Database connection error: ', err);
+  }
+});
+
+db.serialize(function() {
+  //Uncomment to drop tables when restarting the server
+  db.run('DROP TABLE IF EXISTS companies');
+
+  db.run('CREATE TABLE IF NOT EXISTS companies (id INTEGER PRIMARY KEY ASC, name TEXT, created TEXT)');
+});
+
+/**
+ * @param {Object} company - e.g. {name: 'Google'...}
+ * @return {Number} company id
+ */
+db.addCompany = function(company) {
+  
+};
+
+/**
+ * @param  {Number} id - the company id
+ * @return {Null} if company does not exist
+ * @return {Object}  if company is removed successfully
+ */
+db.removeCompany = function(id) {
+
+};
+
+module.exports = db;
