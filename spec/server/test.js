5548c62974646b410300aa0c03dfed2440557e04
Loren Barrick <lorenbarrick@gmail.com>
2016-04-22 15:35:56 -0700

    working on tests

--- spec/server/test.js
+++ spec/server/test.js
@@ -5,9 +5,18 @@ var app = express();
 
 request = request('http://localhost:8080');
 
-describe('should return a 200 when you hit /', function() {
-  it ('respond with a 200 response code', function(done) {
+describe('base root tests', function() {
+  it ('should respond with a 200 response code', function(done) {
     request.get('/').expect(200, function(err) {
+      console.log(err);
+    });
+  });
+});
+
+
+describe('/company route tests', function() {
+  it ('should respond with a 200 status code to show that the company was added', function() {
+    request.post('/company').expect(404, function(err) {
       console.log(err);
     })
     .end(function(err, res) {
