da728d6d5e9e6d53789d265dbe15a5a7fdc56e12
Jake <jake83@gmail.com>
2016-04-23 11:32:23 -0700

    clean up namings

--- client/src/company/index.js
+++ client/src/company/index.js
@@ -1,7 +1,7 @@
 var company = angular.module('jobascript.company', []);
 
 company.config(require('./routes.js'));
-company.controller('CompanyController', require('./controllers.js'));
+company.controller('CompanyController', require('./controller.js'));
 company.factory('Company', require('./services.js'));
 
 module.exports = company;
