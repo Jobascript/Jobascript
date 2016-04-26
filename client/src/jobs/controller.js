63603dffb5ded21a7d5761cfb53574624c12f53d
Loren Barrick <lorenbarrick@gmail.com>
2016-04-26 13:36:47 -0700

    working api call, created routes for back-end appending data from API to client

--- client/src/jobs/controller.js
+++ client/src/jobs/controller.js
@@ -1,11 +1,17 @@
 module.exports = function ($scope, Job, currentCompany) {
-  $scope.jobs = [];
+  $scope.currentJobs = [];
+  $scope.jobDescription = '';
   console.log(currentCompany);
   $scope.jobs = function(){
     Job.getJobs(currentCompany.name).then(function (data) {
       console.log('this is the data', data);
-      return data;
+      $scope.currentJobs = data.listings.listing;
+      // $scope.jobDescription = $scope.currentJobs.description;
+      // console.log($scope.jobDescription);
+      console.log($scope.currentJobs, 'this is jobs');
     });
   };
+
   $scope.jobs(currentCompany.name);
+
 };
--- client/src/jobs/index.js
+++ client/src/jobs/index.js
@@ -1,8 +1,9 @@
-var job = angular.module('jobascript.jobs', ['jobascript.company']);
+var job = angular.module('jobascript.jobs', ['jobascript.company', 'ngSanitize']);
 
 job.config(require('./routes.js'));
 
 job.controller('JobsController', require('./controller.js'));
 job.factory('Job', require('./services.js'));
 
+require('angular-sanitize');
 module.exports = job;
