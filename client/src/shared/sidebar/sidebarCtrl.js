de425f0fae919615ee7d64360f878c99409f310d
Jake <jake83@gmail.com>
2016-04-25 19:22:43 -0700

    end to end add company

--- client/src/shared/sidebar/sidebarCtrl.js
+++ client/src/shared/sidebar/sidebarCtrl.js
@@ -9,6 +9,7 @@ module.exports = function ($scope, Company, companies, $http) {
 
   refreshList = function (id) {
     $scope.companyName = '';
+    console.log(id);
     Company.getCompany(id)
     .then(function (newCompany) {
       console.log('new: ', newCompany);
@@ -16,24 +17,41 @@ module.exports = function ($scope, Company, companies, $http) {
     });
   };
 
-  $scope.addCompany = function (companyName) {
-    console.log('to be added: ', companyName);
-    Company.addCompany({
-      name: inflection.dasherize(angular.lowercase(companyName))
-    })
+  $scope.addCompany = function (company) {
+    company = angular.extend(company, {
+      name: inflection.dasherize(angular.lowercase(company.name)),
+      displayName: company.name
+    });
+
+    console.log('to be added: ', company);
+
+    Company.addCompany(company)
     .then(function (id) {
-      console.log(companyName, ' added as id: ', id);
+      console.log(company.name, ' added as id: ', id);
       return id;
     })
-    .then(refreshList);
+    .then(refreshList)
+    .then(function () {
+      $scope.suggestions = [];
+    });
   };
 
   $scope.findCompanies = _.debounce(function (queryStr) {
+    if (!queryStr) {
+      $scope.suggestions = [];
+      return;
+    }
+
     $http.get(clearbitUrl, {
       params: { query: queryStr }
     }).then(function (resp) {
-      console.log(resp.data);
-      $scope.suggestions = resp.data;
+      var domains = $scope.companies.map(function (company) {
+        return company.domain;
+      });
+
+      $scope.suggestions = _.reject(resp.data, function (company) {
+        return _.contains(domains, company.domain);
+      });
     });
   }, 200);
 };
