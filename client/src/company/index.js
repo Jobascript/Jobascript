a0825b8c6d9bd2520c5f885f4e1f4e58c5e940b4
Jake <jake83@gmail.com>
2016-04-26 11:35:05 -0700

    auto load jobs tab

--- client/src/company/index.js
+++ client/src/company/index.js
@@ -1,6 +1,7 @@
 var company = angular.module('jobascript.company', []);
 
-company.config(require('./routes.js'));
+company.config(require('./routes.js').config);
+company.run(require('./routes.js').listen);
 company.controller('CompanyController', require('./controller.js'));
 company.factory('Company', require('./services.js'));
 
