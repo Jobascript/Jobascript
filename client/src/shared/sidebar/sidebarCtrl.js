0c43049bf9cb4e5a06bce3f9390e83ab61e30f28
Justin Lim <liminator1106@gmail.com>
2016-04-26 09:29:30 -0700

    for rebase

--- client/src/shared/sidebar/sidebarCtrl.js
+++ client/src/shared/sidebar/sidebarCtrl.js
@@ -1,17 +1,10 @@
 var inflection = require('inflection');
 var _ = require('underscore');
-<<<<<<< 63ddb9db9db700b4f98e2ae2f2766b536b6b440b
 var clearbitUrl = 'https://autocomplete.clearbit.com/v1/companies/suggest';
 
 module.exports = function ($scope, Company, companies, $http) {
   $scope.companies = companies;
   $scope.suggestions = [];
-=======
-
-module.exports = function ($scope, $http, Company, companies) {
-  $scope.companies = companies;
-  $scope.option = [];
->>>>>>> db test, remove company working, and dropdown autocomplete working
 
   var findCompanies = _.debounce(function (queryStr) {
     if (!queryStr) {
@@ -62,36 +55,6 @@ module.exports = function ($scope, $http, Company, companies) {
     });
   }
 
-<<<<<<< 63ddb9db9db700b4f98e2ae2f2766b536b6b440b
   $scope.findCompanies = findCompanies;
   $scope.addCompany = addCompany;
-};
-=======
-  $scope.addCompany = function (companyName) {
-    console.log('to be added: ', companyName);
-    Company.addCompany({
-      name: inflection.dasherize(angular.lowercase(companyName))
-    })
-    .then(function (id) {
-      console.log(companyName, ' added as id: ', id);
-      return id;
-    })
-    .then(refreshList);
-  };
->>>>>>> db test, remove company working, and dropdown autocomplete working
-
-  $scope.potentialCompanies = _.debounce(function (companyName) {
-    if (companyName === '') {
-      $scope.option = [];
-    } else {
-      $http({
-        method: 'GET',
-        url: 'https://autocomplete.clearbit.com/v1/companies/suggest?query=' + companyName
-      })
-      .then(function (resp) {
-        $scope.option = resp.data;
-        console.log(resp.data);
-      });
-    }
-  });
 };
