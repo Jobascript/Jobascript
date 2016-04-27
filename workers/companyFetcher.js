7cc57d098b91524ec95b2628f49d9c87dbdca33f
Jake <jake83@gmail.com>
2016-04-26 19:29:19 -0700

    basic cron job

--- workers/companyFetcher.js
+++ workers/companyFetcher.js
@@ -6,33 +6,63 @@ var clearbit = require('clearbit')(config.clearbitKey);
 var db = require('../server/db.js')(config);
 
 var Company = clearbit.Company;
+var CronJob = require('cron').CronJob;
 
-db.getCompanies()
-.then(function (companies) {
-  return Promise.map(companies, apiLookup);
-})
-.then(function (companiesArray) {
-  // console.log('arrComapy: ', companiesArray);
-  return Promise.each(companiesArray, function (richCompany) {
-    db.updateCompany({
-      domain: richCompany.domain
-    }, _.pick(richCompany, 'legalName', 'description', 'location', 'foundedDate', 'url'));
-  });
+var job = new CronJob({
+  cronTime: '* * * * *', // At every minute.
+  onTick: runScript,
+  start: false,
+  timeZone: 'America/Los_Angeles'
 });
+job.start();
 
-function apiLookup(company) {
-  // console.log(company.domain);
-  return Company.find({
-    domain: company.domain,
-    company_name: company.name
+function runScript() {
+  console.log('================================================');
+  console.log('Find companies with incomplete meta data...');
+  console.log('================================================');
+
+  db.getCompanies()
+  .then(function (companies) {
+    console.log('Found: ', companies.length);
+    console.log('Start fetching...');
+    console.log('________________________________________________');
+    return Promise.map(companies, apiLookup);
   })
-  .catch(Company.QueuedError, function (err) {
-    console.log(err); // Company is queued
-  })
-  .catch(Company.NotFoundError, function (err) {
-    console.log(err); // Company could not be found
-  })
-  .catch(function (err) {
-    console.log('Bad/invalid request, unauthorized, Clearbit error, or failed request');
+  .then(function (companiesArray) {
+    console.log('________________________________________________');
+    console.log('fetched: ', companiesArray.length);
+    console.log('Start updating database...');
+    console.log('________________________________________________');
+    return Promise.each(companiesArray, function (richCompany) {
+    console.log('update company: ', richCompany.domain);
+      db.updateCompany({
+        domain: richCompany.domain
+      }, _.pick(richCompany, 'legalName', 'description', 'location', 'foundedDate', 'url'));
+    });
+  }).catch(function (err) {
+    console.log('***=============================================');
+    console.log('*** ERROR: ', err);
+    console.log('***=============================================');
+  }).finally(function () {
+    console.log('================================================');
+    console.log('Completed without error');
+    console.log('================================================');
   });
+
+  function apiLookup(company) {
+    console.log('fetching... ', company.domain);
+    return Company.find({
+      domain: company.domain,
+      company_name: company.name
+    })
+    .catch(Company.QueuedError, function (err) {
+      console.log(err); // Company is queued
+    })
+    .catch(Company.NotFoundError, function (err) {
+      console.log(err); // Company could not be found
+    })
+    .catch(function (err) {
+      console.log('Bad/invalid request, unauthorized, Clearbit error, or failed request');
+    });
+  }
 }
