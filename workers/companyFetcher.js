79cb0d2291eeb0199871bd0584eabcf55d529c50
Jake <jake83@gmail.com>
2016-04-26 17:45:56 -0700

    fetch companies and update db

--- workers/companyFetcher.js
+++ workers/companyFetcher.js
@@ -1,6 +1,6 @@
 var config = require('../server/common.js').config();
 
-var Promise = require("bluebird");
+var Promise = require('bluebird');
 var _ = require('underscore');
 var clearbit = require('clearbit')(config.clearbitKey);
 var db = require('../server/db.js')(config);
@@ -12,16 +12,12 @@ db.getCompanies()
   return Promise.map(companies, apiLookup);
 })
 .then(function (companiesArray) {
-  console.log('arrComapy: ', companiesArray);
-})
-.catch(Company.QueuedError, function (err) {
-  console.log(err); // Company is queued
-})
-.catch(Company.NotFoundError, function (err) {
-  console.log(err); // Company could not be found
-})
-.catch(function (err) {
-  console.log('Bad/invalid request, unauthorized, Clearbit error, or failed request');
+  // console.log('arrComapy: ', companiesArray);
+  return Promise.map(companiesArray, function (richCompany) {
+    return db.updateCompany({
+      domain: richCompany.domain
+    }, _.pick(richCompany, 'legalName', 'description', 'location', 'foundedDate', 'url'));
+  });
 });
 
 function apiLookup(company) {
@@ -29,5 +25,14 @@ function apiLookup(company) {
   return Company.find({
     domain: company.domain,
     company_name: company.name
+  })
+  .catch(Company.QueuedError, function (err) {
+    console.log(err); // Company is queued
+  })
+  .catch(Company.NotFoundError, function (err) {
+    console.log(err); // Company could not be found
+  })
+  .catch(function (err) {
+    console.log('Bad/invalid request, unauthorized, Clearbit error, or failed request');
   });
 }
