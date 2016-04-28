4e68e877f3f1676181baa44d7686509cda419a7c
Jake <jake83@gmail.com>
2016-04-28 14:12:42 -0700

    sidebar filter

--- client/src/company/controller.js
+++ client/src/company/controller.js
@@ -6,7 +6,6 @@ module.exports = function ($scope, currentCompany, Company, $state) {
   var idArray = Company.getList().map(function (com) {
     return com.id;
   });
-  // console.log(Company.getList());
 
   $scope.isFollowing = _(idArray).contains(currentCompany.id);
 
@@ -15,18 +14,13 @@ module.exports = function ($scope, currentCompany, Company, $state) {
   $scope.unfollow = unfollowCompany;
 
   function unfollowCompany() {
-    // if (confirm('Are you sure?')){ // eslint-disable-line
-      Company.unfollow(currentCompany);
-      console.log('ctrl ', Company.getList());
-      $state.go('company', Company.getList()[0], { reload: false });
-    // }
+    Company.unfollow(currentCompany);
+    $state.go('company', Company.getList()[0], { reload: false });
   }
 
   function followCompany() {
-    console.log('follow: ', currentCompany);
-
     Company.follow(currentCompany);
-    console.log(Company.getList());
+    $scope.isFollowing = true;
   }
 };
 
--- client/src/shared/sidebar/sidebarCtrl.js
+++ client/src/shared/sidebar/sidebarCtrl.js
@@ -1,59 +1,56 @@
-var inflection = require('inflection');
-var _ = require('underscore');
-
-module.exports = function ($scope, Company, companies, $http, $state) {
+module.exports = function ($scope, Company, companies) {
   $scope.companies = Company.getList;
-  $scope.suggestions = [];
+  // $scope.suggestions = [];
 
   // methods
-  $scope.suggest = _.debounce(suggestCompanies, 200);
-  $scope.addCompany = addCompany;
+  // $scope.suggest = _.debounce(suggestCompanies, 200);
+  // $scope.addCompany = addCompany;
 
-  function suggestCompanies(queryStr) {
-    if (queryStr === '') {
-      $scope.suggestions = [];
-      return;
-    }
+  // function suggestCompanies(queryStr) {
+  //   if (queryStr === '') {
+  //     $scope.suggestions = [];
+  //     return;
+  //   }
 
-    Company.suggest(queryStr)
-    .then(function (resp) {
-      var domains = $scope.companies.map(function (company) {
-        return company.domain;
-      });
+  //   Company.suggest(queryStr)
+  //   .then(function (resp) {
+  //     var domains = $scope.companies.map(function (company) {
+  //       return company.domain;
+  //     });
 
-      $scope.suggestions = _.reject(resp.data, function (company) {
-        return _.contains(domains, company.domain);
-      });
-    });
-  }
+  //     $scope.suggestions = _.reject(resp.data, function (company) {
+  //       return _.contains(domains, company.domain);
+  //     });
+  //   });
+  // }
 
-  function addCompany(company) {
-    console.log('adding: ', company);
-    // Removing non-alphanumeric chars
-    console.log(company.name);
-    var name = inflection.dasherize(angular.lowercase(company.name).replace(/[^0-9a-zA-Z\s]/g, ''));
-    angular.extend(company, {
-      name: name,
-      displayName: company.name
-    });
+  // function addCompany(company) {
+  //   console.log('adding: ', company);
+  //   // Removing non-alphanumeric chars
+  //   console.log(company.name);
+  //   var name = inflection.dasherize(angular.lowercase(company.name).replace(/[^0-9a-zA-Z\s]/g, ''));
+  //   angular.extend(company, {
+  //     name: name,
+  //     displayName: company.name
+  //   });
 
-    console.log('to be added: ', company);
+  //   console.log('to be added: ', company);
 
-    Company.addCompany(company)
-    .then(function (id) {
-      console.log(company.name, ' added as id: ', id);
-      return id;
-    })
-    .then(refreshList)
-    .then(clearSuggestions);
-  }
+  //   Company.addCompany(company)
+  //   .then(function (id) {
+  //     console.log(company.name, ' added as id: ', id);
+  //     return id;
+  //   })
+  //   .then(refreshList)
+  //   .then(clearSuggestions);
+  // }
 
-  function clearSuggestions() {
-    $scope.suggestions = [];
-  }
+  // function clearSuggestions() {
+  //   $scope.suggestions = [];
+  // }
 
-  function refreshList() {
-    $scope.companyName = '';
-    $state.reload();
-  }
+  // function refreshList() {
+  //   $scope.companyName = '';
+  //   $state.reload();
+  // }
 };
