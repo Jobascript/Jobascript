8cb8d806856ae3709aa745aefbc19033de0b7be7
Ivan Meza <ivn.mza@gmail.com>
2016-04-28 17:01:36 -0700

    Appears to be making succesful call, albeit sans one requisite param

--- client/src/communications/controller.js
+++ client/src/communications/controller.js
@@ -1,5 +1,15 @@
 module.exports = function ($scope, Comm, currentCompany) {
   console.log(currentCompany);
+  $scope.runScript = (function() {
+    var googleScript = document.createElement('script');
+    googleScript.setAttribute('src', 'https://apis.google.com/js/client.js');
+    googleScript.setAttribute('id', 'onetime');
+
+    if(!document.getElementById('onetime')){
+      document.head.appendChild(googleScript);
+    }
+  })();
+
   $scope.emails = Comm.getEmails();
   $scope.auth = Comm.checkAuth;
 };
--- client/src/communications/services.js
+++ client/src/communications/services.js
@@ -1,14 +1,15 @@
 // eslint-disable-next-line no-unused-vars
 module.exports = function ($http) { // remove comment and use $http later
-  var CLIENT_ID = '474025464808-fhpb5lbhf76e1r4jhogdr8e3h8f2lalb.apps.googleusercontent.com';
+  var CLIENT_ID = '647322278471-06e71cofl2ddsauer9rtoopfpokgo4pm.apps.googleusercontent.com';
   var SCOPES = ['https://www.googleapis.com/auth/gmail.compose'];
 
   var checkAuth = function () {
-    console.log('sending auth request');
+    console.log('1: Initiating auth process');
+    console.log('2: Requesting authorization status from google');
     gapi.auth.authorize({
       client_id: CLIENT_ID,
       scope: SCOPES.join(' '),
-      immediate: true }, handleAuthResult);
+      immediate: false }, handleAuthResult);
   };
 /**
   * Handle response from authorization server.
@@ -16,16 +17,22 @@ module.exports = function ($http) { // remove comment and use $http later
   * @param {Object} authResult Authorization result.
   */
   function handleAuthResult(authResult) {
+    console.log('3: Authorization status result received');
+
     var authorizeDiv = document.getElementById('authorize-div');
     if (authResult && !authResult.error) {
       // Hide auth UI, then load client library.
       authorizeDiv.style.display = 'none';
-      console.log('user has authorized client');
+      console.log('4.success: User has authorized client');
+      // If a callback is not provided, a promise is returned.
+      console.log('5.success: Loading the client library interface to Gmail API')
       loadGmailApi();
+      console.log('Loading call has returned')
     } else {
       // Show auth UI, allowing the user to initiate authorization by
       // clicking authorize button.
-      console.log('user has not authorized client');
+      console.log('4.fail: User has not authorized client');
+      console.log('Should redirect to a login portal');
       authorizeDiv.style.display = 'inline';
     }
   }
@@ -34,34 +41,72 @@ module.exports = function ($http) { // remove comment and use $http later
   * is loaded.
   */
   function loadGmailApi() {
-    console.log('loading gmail api client library');
-    gapi.client.load('gmail', 'v1', listLabels);
+    console.log('6.success: Request to access gmail api granted')
+    gapi.client.load('gmail', 'v1').then(function(){
+      console.log('7.success: Building request ')
+      var request = gapi.client.gmail.users.messages.list({
+        'userID': 'me'
+      });
+      console.log('request: ', request);
+      request.then(function(resp) {
+      console.log('8.success: sending request');
+      console.log('resp: ', resp);
+      })
+    });
+    //gapi.client.load('gmail', 'v1', listMessages('me', 'github', appendPre));
   }
 
+
+  // function listMessages(userId, query, callback) {
+  //   console.log('inside list messages');
+  //   // what is the format of result?
+  //   var getPageOfMessages = function (request, result) {
+  //     console.log('inside getPageofMessages');
+  //     request.execute(function (resp) {
+  //       result = result.concat(resp.messages);
+  //       var nextPageToken = resp.nextPageToken;
+  //       if (nextPageToken) {
+  //         request = gapi.client.gmail.users.messages.list({
+  //           userId: userId,
+  //           pageToken: nextPageToken,
+  //           q: query
+  //         });
+  //         getPageOfMessages(request, result);
+  //       } else {
+  //         callback(result);
+  //       }
+  //     });
+  //   };
+  //   var initialRequest = gapi.client.gmail.users.messages.list({
+  //     user: userId,
+  //     q: query
+  //   });
+  //   getPageOfMessages(initialRequest, []);
+  // }
 /**
   * Print all Labels in the authorized user's inbox. If no labels
   * are found an appropriate message is printed.
   */
-  function listLabels() {
-    console.log('listing label');
-    var request = gapi.client.gmail.users.labels.list({
-          'userId': 'me'
-        });
-    request.execute(function (resp) {
-      var labels = resp.labels;
-      appendPre('Labels:');
-      if (labels && labels.length > 0) {
-        console.log('has labels');
-        for (var i = 0; i < labels.length; i++) {
-          var label = labels[i];
-          appendPre(label.name);
-        }
-      } else {
-        console.log('has no labels');
-        appendPre('No Labels found.');
-      }
-    });
-  }
+  // function listLabels() {
+  //   console.log('listing label');
+  //   var request = gapi.client.gmail.users.labels.list({
+  //         'userId': 'me'
+  //       });
+  //   request.execute(function (resp) {
+  //     var labels = resp.labels;
+  //     appendPre('Labels:');
+  //     if (labels && labels.length > 0) {
+  //       console.log('has labels');
+  //       for (var i = 0; i < labels.length; i++) {
+  //         var label = labels[i];
+  //         appendPre(label.name);
+  //       }
+  //     } else {
+  //       console.log('has no labels');
+  //       appendPre('No Labels found.');
+  //     }
+  //   });
+  // }
 /**
   * Append a pre element to the body containing the given message
   * as its text node.
@@ -75,6 +120,7 @@ module.exports = function ($http) { // remove comment and use $http later
   }
 
   var getEmails = function () {
+    return [1, 2, 3, 4];
   };
 
   return {
