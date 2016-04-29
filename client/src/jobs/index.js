d1c85a97206b17dd5e9e4a5316d0a879ad31c7d4
Loren Barrick <lorenbarrick@gmail.com>
2016-04-28 18:47:54 -0700

    add test suites

--- client/src/jobs/index.js
+++ client/src/jobs/index.js
@@ -1,4 +1,4 @@
-var job = angular.module('jobascript.jobs', ['jobascript.company', 'ngSanitize']);
+var job = angular.module('jobascript.jobs', ['jobascript.company', 'ngSanitize', 'ngAnimate']);
 
 
 
@@ -10,4 +10,5 @@ job.filter('renderHTMLCorrectly', require('./filter.js'));
 
 require('./styles.css');
 require('angular-sanitize');
+require('angular-animate');
 module.exports = job;
