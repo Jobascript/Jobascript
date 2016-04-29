db0d12b8206f2cb369fa78f934baf62986ce7c57
Loren Barrick <lorenbarrick@gmail.com>
2016-04-28 21:19:11 -0700

    start of client test suite

--- spec/client/conf.js
+++
@@ -1,5 +0,0 @@
-exports.config = {
-  framework: 'jasmine',
-  seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
-  specs: ['spec/client/spec.js']
-};
---
+++ spec/client/jobs.js
@@ -0,0 +1,7 @@
+var expect = require('chai').expect;
+
+describe('a test', function() {
+	it('should pass', function() {
+	  expect('hello').to.equal('hello');
+	})
+})
