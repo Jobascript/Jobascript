bd13f44e957f89044878d4dffa4bbf05574580df
Loren Barrick <lorenbarrick@gmail.com>
2016-04-30 12:09:17 -0700

    updated new factory

--- client/src/jobs/routes.js
+++ client/src/jobs/routes.js
@@ -2,6 +2,11 @@ module.exports = function ($stateProvider) {
   $stateProvider.state('jobs', {
     parent: 'company',
     url: '/jobs',
+    resolve: {
+      additionalOptions: function($stateParams) {
+        console.log($stateParams);
+      }
+    },
     controller: 'JobsController',
     template: require('./job.html')
   });
