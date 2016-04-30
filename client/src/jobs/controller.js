5c8eca6333aef5889b558a8f440f7763ed40537d
Loren Barrick <lorenbarrick@gmail.com>
2016-04-29 19:01:16 -0700

    update package.json, add filter for jobs

--- client/src/jobs/controller.js
+++ client/src/jobs/controller.js
@@ -1,5 +1,6 @@
 module.exports = function ($scope, Job, currentCompany) {
   $scope.currentJobs = [];
+  $scope.jobSkills = [];
   $scope.jobDescription = '';
 
   $scope.clicked = function (job) {
@@ -24,3 +25,20 @@ module.exports = function ($scope, Job, currentCompany) {
     console.log('raw data', data);
   });
 };
+
+
+// <div> Required Skills
+//   <li ng-repeat='jobSkills in job.category'>{{jobSkills}}</li>
+// </div>
+
+
+// <div>
+//   <input type='text' placeholder='filter locations' ng-model='jobSearch'/>
+//   <div ng-repeat="job in currentJobs | filter: {title: jobSearch}">
+//     <li>{{decode(job.title)}}</li>
+//     <h4>Apply here <a ng-href='{{job.link}}' target='_blank'>{{job.link}}</a></h4>
+//     <div class='job.publish'>Posted On {{decode(job.pubDate)}}</div>
+//     <div class='description-{{$index}}' ng-click="clicked(job)">Job Description</div>
+//     <li class='job-compact' ng-class="{'job-compact': job.isCompact}" ng-bind-html='decode(job.description)'></d>
+//   </div>
+// </div>
