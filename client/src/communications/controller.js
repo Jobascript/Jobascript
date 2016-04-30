e310b642dfa6d022fa0b420f8cfa80ab6c6ee0b7
Ivan Meza <ivn.mza@gmail.com>
2016-04-30 16:22:57 -0700

    identified issue in data binding

--- client/src/communications/controller.js
+++ client/src/communications/controller.js
@@ -1,6 +1,9 @@
-module.exports = function ($scope, Comm, currentCompany) {
-  console.log(currentCompany);
+module.exports = function ($scope, Comm, currentCompany, emails, $state) {
+  // console.log(currentCompany);
+  // var theEmails = [];
+  $scope.myEmails = emails;
 
+<<<<<<< b7a5f9ad0c18968c296a9c4de983e8b77dd579bd
   var googleScript = document.createElement('script');
   googleScript.setAttribute('src', 'https://apis.google.com/js/client.js');
   googleScript.setAttribute('id', 'onetime');
@@ -16,6 +19,41 @@ module.exports = function ($scope, Comm, currentCompany) {
         console.log('emails here: ', emails);
         $scope.emails = emails;
       });
+=======
+
+  // need to check auth first
+  
+
+  // $scope.auth = function() {
+  //   console.log('check auth');
+  //   Comm.checkAuth().then(function () {
+  //     console.log('get emails');
+  //     Comm.getEmails(currentCompany).then(function (emails) {
+  //       console.log('emails here: ', emails);
+  //       $scope.emails = emails;
+  //       console.log('$scope.emails ', $scope.emails);
+  //     });
+  //   });
+  // };
+
+  $scope.auth = function () {
+    Comm.checkAuth()
+    .then(function() {
+      console.log('authoized');
+      $state.reload();
+>>>>>>> identified issue in data binding
     });
   };
+
+  $scope.getEmails = function () {
+    Comm.getEmails(currentCompany).then(function (emails) {
+      console.log('b ', $scope.myEmails);
+      updateEmails(emails);
+      console.log('a ', $scope.myEmails);
+    });
+  };
+
+  function updateEmails(emails) {
+    $scope.myEmails = emails;
+  }
 };
--- client/src/communications/routes.js
+++ client/src/communications/routes.js
@@ -2,6 +2,26 @@ module.exports = function ($stateProvider) {
   $stateProvider.state('comm', {
     parent: 'company',
     url: '/communications',
+    resolve: {
+      emails: function (Comm) {
+        return Comm.getEmails(function(emails) {
+          return emails;
+        }, function(err) {
+          console.log('not authorize: ', err);
+          return [];
+        });
+      }
+    },
+    onEnter: function () {
+      var googleScript = document.createElement('script');
+      
+      googleScript.setAttribute('src', 'https://apis.google.com/js/client.js');
+      googleScript.setAttribute('id', 'onetime');
+
+      if (!document.getElementById('onetime')) {
+        document.head.appendChild(googleScript);
+      }
+    },
     controller: 'CommController',
     template: require('./comm.html')
   });
