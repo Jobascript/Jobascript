3651703e6fce1ded26cc74ef74f4a0f8041bfdf8
Ivan Meza <ivn.mza@gmail.com>
2016-04-30 16:22:57 -0700

    identified issue in data binding

--- client/src/communications/controller.js
+++ client/src/communications/controller.js
@@ -2,6 +2,11 @@ module.exports = function ($scope, Comm, currentCompany, emails, $state) {
   // console.log(currentCompany);
   // var theEmails = [];
   $scope.myEmails = emails;
+<<<<<<< 0fbebe1f15ec9c1a6c33d2606282ff29bd67aa0f
+=======
+
+<<<<<<< b7a5f9ad0c18968c296a9c4de983e8b77dd579bd
+>>>>>>> identified issue in data binding
   var googleScript = document.createElement('script');
   googleScript.setAttribute('src', 'https://apis.google.com/js/client.js');
   googleScript.setAttribute('id', 'onetime');
@@ -17,6 +22,29 @@ module.exports = function ($scope, Comm, currentCompany, emails, $state) {
         console.log('emails here: ', emails);
         $scope.emails = emails;
       });
+=======
+
+  // need to check auth first
+  
+
+  // $scope.auth = function() {
+  //   console.log('check auth');
+  //   Comm.checkAuth().then(function () {
+  //     console.log('get emails');
+  //     Comm.getEmails(currentCompany).then(function (emails) {
+  //       console.log('emails here: ', emails);
+  //       $scope.emails = emails;
+  //       console.log('$scope.emails ', $scope.emails);
+  //     });
+  //   });
+  // };
+
+  $scope.auth = function () {
+    Comm.checkAuth()
+    .then(function() {
+      console.log('authoized');
+      $state.reload();
+>>>>>>> identified issue in data binding
     });
   };
 
