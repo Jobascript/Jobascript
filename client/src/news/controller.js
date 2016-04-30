a01aa2149c69d531ac43ec88c73ae0f06563fff0
Justin Lim <liminator1106@gmail.com>
2016-04-30 16:29:43 -0700

    added target=_blank to index, added filter to news tab

--- client/src/news/controller.js
+++ client/src/news/controller.js
@@ -4,6 +4,7 @@ module.exports = function ($scope, $sce, News, currentCompany) {
   $scope.getNews = function (query) {
     News.getGoogleNews(query)
     .then(function (data) {
+      console.log('data', data);
       $scope.news = data.feed.entries;
       if ($scope.news.length === 0) {
         $scope.news = [{ content: 'Sorry, there is no news for that topic' }];
