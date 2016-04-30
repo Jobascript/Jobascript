5c6415da91cbcc54774c077359ed609c6a92ba46
Loren Barrick <lorenbarrick@gmail.com>
2016-04-30 12:09:17 -0700

    updated new factory

--- client/src/jobs/services.js
+++ client/src/jobs/services.js
@@ -1,10 +1,10 @@
 module.exports = function ($http) {
    // remove comment and use $http later
-  function getJobs(company) {
+  function getJobs(company, relocate, visa, remote) {
     return $http({
       method: 'GET',
       url: '/api/jobs',
-      params: { companyName: company }
+      params: { companyName: company, relocate: relocate, visa: visa, remote: remote }
     })
     .then(function (resp) {
       console.log('this is the response', resp.data);
@@ -14,8 +14,24 @@ module.exports = function ($http) {
       console.log('error in services js ', caught);
     });
   }
+  function getJobListing(listing) {
+    console.log("gets here");
+    return $http({
+      method: 'GET',
+      url: '/api/job',
+      params: { jobListing: listing }
+    })
+    .then(function (resp) {
+      console.log('this is the jobListing resp', resp.data);
+      return resp.data;
+    })
+    .catch(function (caught) {
+      console.log('error in services js', caught);
+    });
+  }
 
   return {
-    getJobs: getJobs
+    getJobs: getJobs,
+    getJobListing: getJobListing
   };
 };
--- server/job/index.js
+++ server/job/index.js
@@ -1,22 +1,33 @@
 var rp = require('request-promise');
 var parser = require('xml2json');
 
-module.exports = function getJobs(req, res) {
+
+exports.getJobs = function(req, res) {
   console.log(req.query.companyName);
   var companyName = req.query.companyName;
-  rp('https://stackoverflow.com/jobs/feed?searchTerm=' + companyName)
+  var offersRelocation = req.query.relocate;
+  var offersVisa = req.query.visa;
+  var allowsRemote = req.query.remote;
+  console.log(allowsRemote);
+  rp('https://stackoverflow.com/jobs/feed?searchTerm=' + companyName + '&offersrelocation=' + offersRelocation + '&offersvisasponsorship=' + offersVisa + '&allowsremote=' + allowsRemote)
    .then(function(data) {
      var json = parser.toJson(data);
-    //  console.log(data);
      res.send(json);
    })
    .catch(function(err) {
     console.log('error in index.js job', err);
     res.sendStatus(500);
   });
+}
+
+exports.getJobListing = function(req, res) {
+  console.log('req listing from index.js', req.query.jobListing);
+  res.send('hurro george');
 };
 
 
-// parser.on('title', function(title) {
-//       console.log(title);
-// });
+// http://stackoverflow.com/jobs?
+// searchTerm=twitch&location=san+francisco&range=20&distanceUnits=Miles&
+// allowsremote=true&
+// offersrelocation=true&
+// offersvisasponsorship=true
