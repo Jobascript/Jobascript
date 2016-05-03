29941677eaa15835a3e975ea37737fb9c423c283
Justin Lim <liminator1106@gmail.com>
2016-05-03 10:25:21 -0700

    to rebase

--- client/src/news/services.js
+++ client/src/news/services.js
@@ -1,4 +1,23 @@
 module.exports = function ($http) {
+  var alchemyKey 
+  // = 'ccb8fd2204772690e0260ef3fcdb99772422b22d';
+  // = 'aeacf083250dd17e5e5cc0da04aa316da4e94bfd';
+  = 'f0855469b5be36c6b4ae467290751bf8663f014a';
+
+  var alchemyUrl = 'https://gateway-a.watsonplatform.net/calls/data/GetNews?apikey=' + alchemyKey + '&outputMode=json&start=now-1y&end=now';
+
+  var getAlchemyNews = function (companyName) {
+    return $http({
+      method: 'GET',
+      url: alchemyUrl + '&return=enriched.url.text,enriched.url.url,enriched.url.title,enriched.url.image&dedup=1&rank=high^medium&q.enriched.url.enrichedTitle.entities.entity.text=' + companyName
+    })
+    .then(function (resp) {
+      return resp.data;
+    }, function (err) {
+      console.log('err', err);
+    });
+  };
+
   var getGoogleNews = function (companyName) {
     return $http({
       method: 'GET',
@@ -13,6 +32,7 @@ module.exports = function ($http) {
   };
 
   return {
+    getAlchemyNews: getAlchemyNews,
     getGoogleNews: getGoogleNews
   };
 };
