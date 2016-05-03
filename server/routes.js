a78914555a4d783e00002bf43631ca9b9a032405
Jake <jake83@gmail.com>
2016-05-03 12:30:37 -0700

    set up api for user

--- server/routes.js
+++ server/routes.js
@@ -1,12 +1,19 @@
+var userHandler = require('./user');
 var companyHandler = require('./company');
 var newsHandler = require('./news');
 var jobHandler = require('./job');
 
 module.exports = function (app) {
+  app.post('/api/user', userHandler.createUser);
+  app.post('/api/user/:user_id/company/:company_id', userHandler.followCompany);
+  app.delete('/api/user/:user_id/company/:company_id', userHandler.unfollowCompany);
+
   app.get('/api/company/:id', companyHandler.getCompany);
   app.post('/api/company', companyHandler.addCompany);
   app.delete('/api/company/:id', companyHandler.removeCompany);
   app.get('/api/companies', companyHandler.getCompanies);
+
   app.get('/api/news', newsHandler);
+
   app.get('/api/jobs', jobHandler);
 };
---
+++ server/user/index.js
@@ -0,0 +1,24 @@
+var db = require('../../server/database').usersTable;
+
+exports.followCompany = function (req, res) {
+  // user, company
+  var userID = req.params.user_id;
+  var companyID = req.params.company_id;
+
+  if (!userID || !companyID) {
+    res.status(400).send('user ID and company ID is required');
+  }
+  db.followCompany(userID, companyID).then(function () {
+    res.sendStatus(200);
+  }, function (reason) {
+    res.status(406).send(reason);
+  }).catch(function (err) {
+    res.status(500).send(err);
+  });
+};
+
+// exports.createUser = function (req, res) {
+//   var user = req.body;
+
+//   db.createUser(user);
+// };
