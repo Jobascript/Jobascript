13449c29409b7599748201d142d562a8223897ff
Loren Barrick <lorenbarrick@gmail.com>
2016-05-03 19:55:32 -0700

    update file

--- client/src/jobs/routes.js
+++ client/src/jobs/routes.js
@@ -2,11 +2,6 @@ module.exports = function ($stateProvider) {
   $stateProvider.state('jobs', {
     parent: 'company',
     url: '/jobs',
-    resolve: {
-      additionalOptions: function($stateParams) {
-        console.log($stateParams);
-      }
-    },
     controller: 'JobsController',
     template: require('./job.html')
   });
