cb83027d051e09b7c4618d4dafc7c78324c6ba8a
Loren Barrick <lorenbarrick@gmail.com>
2016-05-03 19:30:00 -0700

    fix syntax errors

--- client/src/jobs/controller.js
+++ client/src/jobs/controller.js
@@ -3,7 +3,7 @@ module.exports = function ($scope, Job, currentCompany) {
   $scope.jobSkills = [];
   $scope.jobDescription = '';
   $scope.details = null;
-  $scope.doStuff = function(listing) {
+  $scope.doStuff = function (listing) {
     console.log(listing);
     Job.getJobListing(listing).then(function (data) {
       console.log('data from getJobListing', data);
@@ -20,8 +20,9 @@ module.exports = function ($scope, Job, currentCompany) {
     return e.childNodes.length === 0 ? '' : e.childNodes[0].nodeValue.replace(/\<br \/\>/g, '');
   };
 
-  $scope.search = function() {
-    Job.getJobs(currentCompany.name, $scope.job_relocation, $scope.job_visa, $scope.job_remote).then(function (data) {
+  $scope.search = function () {
+    Job.getJobs(currentCompany.name, $scope.job_relocation, $scope.job_visa, $scope.job_remote)
+    .then(function (data) {
       $scope.currentJobs = data.rss.channel.item;
       if (!Array.isArray($scope.currentJobs)) {
         $scope.currentJobs = [$scope.currentJobs];
@@ -35,17 +36,8 @@ module.exports = function ($scope, Job, currentCompany) {
       console.log('raw data after clicked', data);
     });
   };
-  Job.getJobs(currentCompany.name).then(function (data) {
+ Job.getJobs(currentCompany.name).then(function (data) {
      $scope.currentJobs = data[0].jobTitle;
-    // var jobArray = data.rss.channel.item;
-    //
-    // for (var i = 0; i < jobArray.length; i++) {
-    //   console.log('jobarray[i]', jobArray[i], 'currentCompany', currentCompany.name, jobArray[i]['a10:author']['a10:name'] === currentCompany.name);
-    //   if (jobArray[i]['a10:author']['a10:name'] === currentCompany.name) {
-    //     $scope.currentJobs.push(data);
-    //   }
-    // }
-    // $scope.currentJobs = data.rss.channel.item;
     if (!Array.isArray($scope.currentJobs)) {
       $scope.currentJobs = [$scope.currentJobs];
       $scope.currentJobs[0].isCompact = true;
