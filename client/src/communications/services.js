82b0435e3b866f094c2bde93c163f0aa2aa99a59
Ivan Meza <ivn.mza@gmail.com>
2016-05-04 20:02:05 -0700

    Emails formatted

--- client/src/communications/services.js
+++ client/src/communications/services.js
@@ -3,6 +3,19 @@ module.exports = function () { // remove comment and use $http later
   var CLIENT_ID = '647322278471-06e71cofl2ddsauer9rtoopfpokgo4pm.apps.googleusercontent.com';
   var SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
   /* globals gapi */
+  // ------------------------------------------------
+  // --------------- Helper Functions ---------------
+  var getHeader = function (headers, headerName) {
+    var header = '';
+    headers.forEach(function (element) {
+      if (element.name === headerName) {
+        header = element.value;
+      }
+    });
+    return header;
+  };
+  // ===============================================
+  // ===============================================
   var checkAuth = function () {
     return new Promise(function (resolve) {
       gapi.auth.authorize({
@@ -31,9 +44,12 @@ module.exports = function () { // remove comment and use $http later
             messageRequest.execute(function (messageResp) {
               // here message is an array
               var headers = messageResp.payload.headers;
+              var date = getHeader(headers, 'Date');
+              var sender = getHeader(headers, 'From');
+              var subject = getHeader(headers, 'Subject');
               // before pushing, extract relevant data to send to view
 
-              messages.push(headers);
+              messages.push('Date: ' + date + ' From: ' + sender + ' Subject: ' + subject);
               if (messages.length === resp.messages.length) {
                 resolve(messages);
               }
@@ -44,15 +60,6 @@ module.exports = function () { // remove comment and use $http later
     });
   };
 
-  var getheader = function (headers, headerName) {
-    var header = '';
-    headers.forEach(function (element) {
-      if (element.name === headerName) {
-        header = element.value;
-      }
-    });
-    return header;
-  };
   return {
     getEmails: getEmails,
     checkAuth: checkAuth
