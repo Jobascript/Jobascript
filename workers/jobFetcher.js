2ffebdaa93cfebd5da226d71cc0de5e98bcd39f7
Loren Barrick <lorenbarrick@gmail.com>
2016-05-07 16:01:42 -0700

    working api call for indeed

--- workers/jobFetcher.js
+++ workers/jobFetcher.js
@@ -6,68 +6,57 @@ var clearbit = require('clearbit')(config.clearbitKey);
 var db = require('../server/database');
 var rp = require('request-promise');
 var jobTable = require('../server/database').jobsTable;
-db.companiesTable.addCompany({name: 'Twitch'});
 db.companiesTable.getCompanies()
+  //promise for getCompanies
   .then(function (companyArr) {
     // console.log('companyArr', companyArr);
-    return Promise.map(companyArr, function(companyObj) {
-      return rp('http://api.indeed.com/ads/apisearch?publisher=9810665890415219&format=json&v=2&q=' + companyObj.name)
-       .then(function (data) {
-         var listOfJobs = JSON.parse(data).results;
-        //  console.log(listOfJobs.results);
-        //  append the proper data to the proper column name in jobs table\
-        //  var listOfJobs = data.results;
-        //  console.log("LIST:", listOfJobs);
-        console.log(companyObj.name);
-         var filteredListOfJobs = _.filter(listOfJobs, function(job, key) {
-          //  console.log('job inside filter', job);
-           console.log("val", job, key)
-           console.log(companyObj.name);
-           var key = job.company.toLowerCase().indexOf(companyObj.name.toLowerCase());
-           console.log("KEY,", key);
-           return key !== -1;
-         });
-         console.log("filtered", filteredListOfJobs);
-       });
+    console.log(companyArr);
+    var companyInfo = _.map(companyArr, function(company) {
+      return [Number(company.id), company.name];
+    });
+    return new Promise(function(resolve, reject) {
+      return Promise.map(companyInfo, function(companyObj) {
+        // console.log(companyObj);
+          return rp('http://api.indeed.com/ads/apisearch?publisher=9810665890415219&format=json&v=2&q=' + companyObj[1])
+           .then(function (data) {
+            //  console.log(companyObj);
+              var listOfJobs = JSON.parse(data).results;
+            //  console.log(listOfJobs);
+            //  console.log(listOfJobs);
+             var filteredListOfJobs = _.filter(listOfJobs, function(job, key) {
+              var filteredJob = job.company.toLowerCase().indexOf(companyObj[1].toLowerCase());
+              // console.log(job.company, 'job company');
+              // console.log('companyObj', companyObj[1]);
+              return filteredJob !== -1;
+             });
+             _.each(filteredListOfJobs, function(jobs) {
+               if (jobs.company === companyObj[1]) {
+                jobs.id = companyObj[0];
+               }
+             });
+            resolve(filteredListOfJobs);
+           });
+      });
     })
     .then(function (data) {
-      // console.log('last then', data);
-      // var newJob = convertKeysToProperKeys(data);
-      // jobTable.addJob(newJob);
-
-    });
-  })
+      _.each(data, function(job) {
+        var resultObj = {
+          remote_ok: null,
+          salary: null,
+          relocation: null,
+          company: job.company,
+          title: job.jobtitle,
+          url: job.url,
+          description: job.snippet,
+          city: job.city,
+          created: job.date,
+          visa_sponsored: job.sponsored,
+          company_id: job.id
+        };
+        jobTable.addJob(resultObj);
+        });
+      });
+    })
   .catch(function (err) {
     console.log('err in jobFetcher.js', err);
   });
-
-
-  function convertKeysToProperKeys(listOfJobs) {
-    var resultArr = [];
-    var resultObj = {};
-    for (var i = 0; i < listOfJobs.length; i++) {
-     var currentItem = listOfJobs[i];
-     for (var key in currentItem) {
-       if (key === 'jobtitle') {
-         resultObj.title = currentItem[key];
-       }
-       if (key === 'url') {
-         resultObj.url = currentItem[key];
-       }
-       if (key === 'snippet') {
-         resultObj.description = currentItem[key];
-       }
-       if (key === 'city') {
-         resultObj.city = currentItem[key];
-       }
-       if (key === 'date') {
-         resultObj.created = currentItem[key];
-       }
-       if (key === 'sponsored') {
-         resultObj.visa_sponsored = currentItem[key];
-       }
-     }
-    }
-   resultArr.push(resultObj);
-   return resultArr;
-  }
