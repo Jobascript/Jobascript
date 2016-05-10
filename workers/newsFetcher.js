2d99265bbd987cc1e2a5cfa0e828cb0a1fd9b489
Justin Lim <liminator1106@gmail.com>
2016-05-10 09:50:06 -0700

    error checks in alchemy news fetcher

--- workers/newsFetcher.js
+++ workers/newsFetcher.js
@@ -4,13 +4,14 @@ var Promise = require('bluebird');
 var parseURL = Promise.promisify(require('rss-parser').parseURL);
 var moment = require('moment');
 var axios = require('axios');
+var _ = require('underscore');
 
 var Companies = require('../server/database').companiesTable;
 var News = require('../server/database').newsTable;
 
 var alchemyKey 
-// = 'ccb8fd2204772690e0260ef3fcdb99772422b22d';
-= 'aeacf083250dd17e5e5cc0da04aa316da4e94bfd';
+= 'ccb8fd2204772690e0260ef3fcdb99772422b22d';
+// = 'aeacf083250dd17e5e5cc0da04aa316da4e94bfd';
 // = 'f0855469b5be36c6b4ae467290751bf8663f014a';
 
 Companies.getCompanies()
@@ -41,7 +42,7 @@ Companies.getCompanies()
       link = link.query.url;
       var articleObj = {
         title: article.title,
-        snippet: article.contentSnippet,
+        snippet: article.contentSnippet.substr(0, 300) + '...',
         url: link,
         date_written: moment(article.pubDate).format()
       };
@@ -61,45 +62,50 @@ Companies.getCompanies()
   return companyInfo;
 })
 .then(function (companyNames) {
-  return new Promise(function (resolve, reject) {
-    Promise.map(companyNames, function (companyName) {
-      var alchemyUrl = 'https://gateway-a.watsonplatform.net/calls/data'
-      var queryParam = '/GetNews?apikey=' + alchemyKey + '&outputMode=json&start=now-1y&end=now';
-      queryParam += '&return=enriched.url.publicationDate.date,enriched.url.text,enriched.url.url,enriched.url.title,enriched.url.image&dedup=1&rank=high^medium&q.enriched.url.enrichedTitle.entities.entity.text=' + companyName[0];
+  return Promise.map(companyNames, function (companyName) {
+    var alchemyUrl = 'https://gateway-a.watsonplatform.net/calls/data'
+    var queryParam = '/GetNews?apikey=' + alchemyKey + '&outputMode=json&start=now-1y&end=now';
+    queryParam += '&return=enriched.url.publicationDate.date,enriched.url.text,enriched.url.url,enriched.url.title,enriched.url.image&dedup=1&rank=high^medium&q.enriched.url.enrichedTitle.entities.entity.text=' + companyName[0];
 
-      var alchemyCall = axios.create({
-        baseURL: alchemyUrl,
-        timeout: 2000
-      });
+    var alchemyCall = axios.create({
+      baseURL: alchemyUrl,
+      timeout: 2000
+    });
 
-      return alchemyCall.get(queryParam)
-      .then(function (results) {
-        if (results.data.result) {
-          for(var i = 0; i < results.data.result.docs.length; i++){
-            results.data.result.docs[i].id = companyName[1];
-          }
+    return alchemyCall.get(queryParam)
+    .then(function (results) {
+      if(results.data.status === 'ERROR') return new Promise.resolve([]);
+
+      if (results.data.result.docs) {
+        for(var i = 0; i < results.data.result.docs.length; i++){
+          results.data.result.docs[i].id = companyName[1];
         }
-        if (results.data.result){
-          resolve(results.data.result.docs);          
-        }          
-      });
+        return results.data.result.docs;          
+      }      
     });
   });
 })
+.then(function (array) {
+  return _.flatten(array);
+})
 .then(function (data) {
+  if(data.length <= 0) return '';
   data.forEach(function (val) {
     var article = val.source.enriched.url;
     var articleObj = {
       title: article.cleanedTitle,
-      snippet: article.text,
+      snippet: article.text.substr(0, 300) + '...',
       url: article.url,
       image_url: article.image,
       date_written: moment(article.publicationDate.date).format(),
       author: article.author
     };
     News.addNews(articleObj, val.id);
-  })
+  });
+  return '';
 })
-.then(function (data) {
+.finally(function (data) {
+  console.log('<<<<<<<<<<<<<<');
+  console.log('>>>>>>>> called');
   News.removeOld();
 });
